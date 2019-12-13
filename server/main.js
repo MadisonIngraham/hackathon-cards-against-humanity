a44b9cdc7f7349a1b8eeaf76d253cd3ac5913d44
Dakota Logan <xeliusone@gmail.com>
2019-12-13 16:08:24 -0700

    Looks good

--- server/main.js
+++ server/main.js
@@ -18,11 +18,11 @@ server.use(express.static(__dirname + "/../client"));
 //NOTE Allows requests from the port 8080, add additional addresses as needed
 var whitelist = ["http://localhost:8080"];
 var corsOptions = {
-  origin: function(origin, callback) {
-    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, originIsWhitelisted);
-  },
-  credentials: true
+	origin: function(origin, callback) {
+		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+		callback(null, originIsWhitelisted);
+	},
+	credentials: true
 };
 server.use(cors(corsOptions));
 
@@ -50,14 +50,14 @@ server.use("/api/users", new usersController().router);
 
 //NOTE Default error handler, catches all routes with an error attached
 server.use((error, req, res, next) => {
-  res.status(error.status || 400).send({ error: { message: error.message } });
+	res.status(error.status || 400).send({ error: { message: error.message } });
 });
 
 //NOTE Catch all to insure to return 404 if recieved a bad route
 server.use((req, res, next) => {
-  res.status(404).send("Route not found");
+	res.status(404).send("Route not found");
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+	console.log(`Server is running on port: ${port}`);
 });
