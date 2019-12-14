ec4b7086b8370af0da186d90914053596d3f83d4
MadisonIngraham <madi.ingraham@gmail.com>
2019-12-13 17:07:17 -0700

    cleanup

--- client/app/Services/PostService.js
+++ client/app/Services/PostService.js
@@ -1,11 +1,8 @@
 import store from "../store.js";
 import Post from "../Models/Post.js";
-
-let _api = axios.create({
-  baseURL: "http://localhost:3000/api",
 //@ts-ignore
 let _api = axios.create({
-  baseURL: "/api/posts",
+  baseURL: "http://localhost:3000/api",
   timeout: 5000
 });
 
@@ -16,7 +13,6 @@ class PostService {
     }
     console.log(store.State.posts);
   }
-
   getPosts() {
     _api
       .get("/posts")
@@ -32,30 +28,9 @@ class PostService {
         console.error(e);
       });
   }
-
-    // for (let i = 0; i < 5; i++) {
-    //   store.State.posts.push(new Post({ title: "lorem ipsum", id: 1 }));
-    // }
-  }
-  // console.log(store.State.posts)
-  getPosts() {
-    _api
-      .get("/posts")
-      .then(res => {
-        console.log("Returned data from get posts:", res);
-        let posts = store.State.posts;
-        console.log("Posts in store:", posts);
-        res.data.map(cur => posts.push(new Post(cur)));
-        console.log("Posts form store and get():", posts);
-        store.commit("posts", posts);
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  }
-  async getRandomBlack() {
-    let res = await _CardsAgainstApi.get("");
-    console.log(res.data.black);
+  async getRandom() {
+    let data = await _api.get("/random");
+    console.log(data);
   }
 
   getPostId(id) {
