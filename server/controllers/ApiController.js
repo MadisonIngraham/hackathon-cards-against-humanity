9908780527462ff535bf15259964031b951a040c
wesquintana <wesquintana@gmail.com>
2019-12-13 18:17:05 -0700

    fixed it

--- server/controllers/ApiController.js
+++ server/controllers/ApiController.js
@@ -1,24 +1,25 @@
 import express from "express";
-import axios from "axios"
+import axios from "axios";
 
 let api = axios.create({
   baseURL: "https://cah-gcs-api.herokuapp.com/api/v1/random?n=1",
   timeout: 3000
-})
+});
 
 export default class RandomApiController {
   constructor() {
     this.router = express
       .Router({ mergeParams: true })
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
-      .get("", this.getAll)
+      .get("", this.getAll);
   }
 
   async getAll(req, res, next) {
     try {
       let data = await api.get("");
       console.log(data);
-      return res.send(data.data.data[0]);
+      // let realData= data.data;
+      return res.send(data.data[0]);
     } catch (error) {
       next(error);
     }
